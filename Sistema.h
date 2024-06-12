094b0db8fae99c10de27a1c185a40539aed454f0
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2024-06-11 22:42:28 -0600

    Sistema.h

--- Sistema.h
+++ Sistema.h
@@ -1,25 +1,23 @@
-#ifndef SISTEMA_H
-#define SISTEMA_H
-
-
-
 #include <iostream>
 #include <string>
 #include "Autoridades.h"
+#ifndef SISTEMA_H
+#define SISTEMA_H
 using namespace std;
 
 
 /*
 Este es el sistema general es el que se 
-va a encargar de la parte de crear las 
+va a encargar de la parte de crear las 7
 autoridades y de manejar el sistema 
-para crear el sistema de agregar incidentes y agregar autoridades ademas de administrar el estatus.
+para crear el sistema de agregar incidentes
+ y agregar autoridades ademas de administrar el estatus.
 
 
 */
 class Sistema{
 private:
-    Autoridades* autoridad[100]; // Array de punteros a Autoridades
+    Autoridades* autoridad[100];
     string ubicacion_incidente;
     string descripcion_incidente;
     string fecha;
@@ -29,13 +27,11 @@ private:
     int n_autoridades;
 		
 public:
-    Sistema() : n_autoridades(0), estatus("No iniciado") {} // Constructor por default 
+    Sistema() : n_autoridades(0), estatus("No iniciado") {}
 
-    // Constructor 
     Sistema(string ubi_in, string des_in, string fec, string hor_in, string niv, string est)
         : ubicacion_incidente(ubi_in), descripcion_incidente(des_in), fecha(fec), hora(hor_in), nivel_gravedad(niv), estatus(est), n_autoridades(0) {}
 
-    // Getters 
     string get_ubicacion_incidente() { return ubicacion_incidente; }
     string get_descripcion_incidente() { return descripcion_incidente; }
     string get_fecha() { return fecha; }
@@ -44,7 +40,6 @@ public:
     string get_estatus() { return estatus; }
     int get_n_autoridades() { return n_autoridades; }
 		
-    // Setters
     void set_ubicacion_incidente(string ubi_in) { ubicacion_incidente = ubi_in; }
     void set_descripcion_incidente(string des_in) { descripcion_incidente = des_in; }
     void set_fecha(string fec) { fecha = fec; }
@@ -53,10 +48,9 @@ public:
     void set_estatus(string est) { estatus = est; }
     void set_n_autoridades(int n_auto) { n_autoridades = n_auto; }
 		
-    // Funciones extras
     void alertar_autoridades() {
         cout << "Las autoridades han sido alertadas y van en camino a la ubicacion " << get_ubicacion_incidente() << endl;
-        estatus = "En progreso"; // Actualizamos el estatus
+        estatus = "En progreso";
     }
 
     void agregar_autoridades(Autoridades* nueva_autoridad) {
@@ -76,13 +70,37 @@ public:
         } else {
             nivel_gravedad = "Bajo";
         }
-        cout << "Nivel de gravedad: " << nivel_gravedad << endl; // Mostramos el nivel de gravedad
+        cout << "Nivel de gravedad: " << nivel_gravedad << endl;
     }
 
     void finalizar_emergencia() {
         cout << "La emergencia ha finalizado con exito" << endl;
-        estatus = "Finalizada"; // Actualizamos el estatus
-        cout << "Estatus: " << estatus << endl; // Mostramos el estatus
+        estatus = "Finalizada";
+        cout << "Estatus: " << estatus << endl;
+    }
+    
+    void imprimir_autoridades() {
+        cout << "Autoridades registradas:" << endl;
+        for (int i = 0; i < n_autoridades; ++i) {
+            cout << "Autoridad " << i + 1 << ":" << endl;
+            autoridad[i]->atender_llamada();
+            autoridad[i]->encender_cirena();
+            cout << "Lugar: " << autoridad[i]->get_lugar() << endl;
+            cout << "Descripción: " << autoridad[i]->get_descripcion() << endl;
+            cout << "---------------------" << endl;
+        }
+    }
+    
+    void buscar_autoridades() {
+        int buscar;
+        cout << "Digite el número de autoridad a buscar: ";
+        cin >> buscar;
+        if (buscar > 0 && buscar <= n_autoridades) {
+            cout << "Descripción de la autoridad: " << autoridad[buscar - 1]->get_descripcion() << endl;
+            cout << "Lugar de la autoridad: " << autoridad[buscar - 1]->get_lugar() << endl;
+        } else {
+            cout << "Número de autoridad no válido." << endl;
+        }
     }
 };
 
